61083dbc69038047e5b445a99297c00e1f4b9714
aasaparin <c5zX93NPaEcK@>
2022-08-16 16:34:58 +0300

    add OpenAPIDefinition

--- src/main/java/edu/school21/restful/ResTfulApplication.java
+++ src/main/java/edu/school21/restful/ResTfulApplication.java
@@ -1,9 +1,22 @@
 package edu.school21.restful;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+    info = @Info(title = "ResTful school 21 project Docs",
+        version = "1.0",
+        description = "This is the official documentation of the ResTful API developed by jmontagu",
+        license = @License(name = "Apache 2.0",
+            url = "https://www.apache.org/licenses/LICENSE-2.0"),
+        contact = @Contact(email = "ghoshalshankhadeep@hotmail.com",
+            name = "Shankhadeep Ghoshal"))
+)
 public class ResTfulApplication {
 
     public static void main(String[] args) {
---
+++ src/main/java/edu/school21/restful/configs/SwaggerConfig.java
@@ -0,0 +1,10 @@
+package edu.school21.restful.configs;
+
+import io.swagger.v3.oas.annotations.info.Contact;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+}
