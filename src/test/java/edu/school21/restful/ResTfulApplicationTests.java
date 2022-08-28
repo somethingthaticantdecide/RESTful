8898231168ab66469ad81d06e3848432f006897e
somethingthaticantdecide <somethingthaticantdecide@gmail.com>
2022-08-28 23:01:43 +0300

    next

--- src/test/java/edu/school21/restful/ResTfulApplicationTests.java
+++ src/test/java/edu/school21/restful/ResTfulApplicationTests.java
@@ -1,13 +1,31 @@
 package edu.school21.restful;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
-class ResTfulApplicationTests {
+import static org.hamcrest.Matchers.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc //need this in Spring Boot test
+public class ResTfulApplicationTests {
+
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
+    public void findOne() throws Exception {
+
     }
 
 }
